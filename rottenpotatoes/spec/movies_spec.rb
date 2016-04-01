3129dff8cc3047d98a2ea1064fa7846e1df72f22
noemiekpn <noemiekpn@gmail.com>
2016-04-01 01:19:15 -0500

    Added director field to new movie and created spec files

---
+++ rottenpotatoes/spec/movies_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe MoviesController, :pending => true do
+  before :each do
+    @movie = Movie.new('Finding Nemo', 'G', '2003-05-03', 'Andrew Stanton')
+  end
+  
+  describe 'create a new movie' do
+    it 'should '
+    end
+  end
+  
+  describe 'edit an existing movie' do
+  end
+  
+  
+  describe 'delete an existing movie' do
+  	it 'should 
+  end
+  
+  describe 'find movies with the same director' do
+    context 'when movie has known director' do
+    end
+    
+    context 'when movie has unknown director' do
+    end
+  end
+  
+end
